b3a964dfa8625a9832d5992c6cb814777f45c836
Y1D0 <73546478+YigitArda-00@users.noreply.github.com>
2022-07-29 02:22:40 +0300

    menü bitti ağla kaşar

---
+++ Assets/script/menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    public void yenidenbasla()
+    {
+        SceneManager.LoadScene("mainscene");
+    }
+}
